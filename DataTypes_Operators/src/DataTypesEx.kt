3a2a482095094bac60ed16b6efd01c21cdaf513b
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2024-09-24 10:53:03 +0530

    Start String

---
+++ DataTypes_Operators/src/DataTypesEx.kt
@@ -0,0 +1,18 @@
+
+      fun main(args : Array<String>)
+      {
+          var name : String = "Shubham" // OR // var name = "Shubham"
+          var isAlive : Boolean = true // OR // var isAlive = true
+          var gender : Char = 'M' // OR // var gender = 'M'
+          var age : Int = 24 // OR // var age = 24
+          var marks : Float = 78.3F // OR // var marks= 78.3F
+          var percentage : Double = 67.56 // OR // var percentage = 67.56
+
+          println("name : " + name)
+          println("isAlive : " +  isAlive)
+          println("gender: " + gender)
+          println("age : " + age)
+          println("marks : " + marks)
+          println("percentage : " + percentage)
+
+      }
