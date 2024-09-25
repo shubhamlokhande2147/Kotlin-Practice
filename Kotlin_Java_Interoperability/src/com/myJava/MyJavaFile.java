083e75a7a4be9066f7ddd455a59454dfc75edc94
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2024-09-25 18:41:47 +0530

    Kotlin and Java Interoperability

---
+++ Kotlin_Java_Interoperability/src/com/myJava/MyJavaFile.java
@@ -0,0 +1,13 @@
+    public class MyJavaFile {
+
+        public static void main(String[] args)
+        {
+            int sum = MyFirstKt.add(34,45); //call here add() function from kotlin
+            System.out.println("Sum from kotlin file : " + sum);
+        }
+
+        public static int sub(int a, int b) //method definition
+        {
+            return a - b;
+        }
+    }
