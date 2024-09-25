a6736d477db4d46e4a521e4efbc751ed7d1ae6ad
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2024-09-25 17:33:04 +0530

    Relational Operators

--- DataTypes_Operators/src/Reading_Data.kt
+++ DataTypes_Operators/src/Reading_Data.kt
@@ -1,39 +1,39 @@
 
-fun main(args : Array<String>)
-{
-    println("enter any string : ")
-    val line = readln()
-    println("string : $line")
+    fun main(args : Array<String>)
+    {
+        println("enter any string : ")
+        val line = readln()
+        println("string : $line")
 
-    //get numeric data from user.here use toInt()
+        //get numeric data from user.here use toInt()
 
-    println("enter the age ")
-    val age = readln().toInt()
-    println("age : $age")
+        println("enter the age ")
+        val age = readln().toInt()
+        println("age : $age")
 
-    //larger number.here use toLong()
+        //larger number.here use toLong()
 
-    println("How much is your yacht worth?")
-    val cost = readln().toLong()
-    println("You entered: ")
-    println(cost)
+        println("How much is your yacht worth?")
+        val cost = readln().toLong()
+        println("You entered: ")
+        println(cost)
 
-    //toDouble()
+        //toDouble()
 
-    println("Enter any double type number:")
-    val number = readln().toDouble()
-    println("You entered the number: ")
-    println(number)
+        println("Enter any double type number:")
+        val number = readln().toDouble()
+        println("You entered the number: ")
+        println(number)
 
-    // toBoolean()
-    println("The earth is flat. Type true or false:")
-    val answer = readln().toBoolean()
-    println("The earth is flat: ")
-    println(answer)
+        // toBoolean()
+        println("The earth is flat. Type true or false:")
+        val answer = readln().toBoolean()
+        println("The earth is flat: ")
+        println(answer)
 
-    //Reading multiple values in one line
-    println("enter two strings(with space) :")
-    val (a, b) = readln().split(" ")
-    println(a)
-    println(b)
-}
+        //Reading multiple values in one line
+        println("enter two strings(with space) :")
+        val (a, b) = readln().split(" ")
+        println(a)
+        println(b)
+    }
