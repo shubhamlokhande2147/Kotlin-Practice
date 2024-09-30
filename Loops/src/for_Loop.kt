cce47689d51633837b8102ba5b92970a782900e0
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2024-09-30 16:12:22 +0530

    for Loop another Example

--- Loops/src/for_Loop.kt
+++ Loops/src/for_Loop.kt
@@ -34,7 +34,54 @@
         }
 
         //---------------------------
-        
+        //sum of 1 to 100
+        var sum = 0
+        for(i in 1..100)
+        {
+            sum = sum + i
+        }
+        println(sum)
 
+        //---------------------------------
+        //print A to Z
+        for(i in 'A'..'Z')
+        {
+            println(i)
+        }
 
+        //----------------
+
+        var abc = "shubham"
+        for (ch in abc)
+            println(ch)
+
+        //----------------------------
+        //Iterating in the backward order
+         for(i in 4 downTo 1)
+         {
+             println(i)
+         }
+
+        //------------------------------------
+        //Excluding the upper limit
+        for(i in 1 until 4)
+        {
+            println(i)
+        }
+
+        //------------------------------------
+        //Specifying a step
+        for(i in 1..10 step 2)
+        {
+            println(i)
+        }
+
+        //---------------------------
+        //print table of 2
+
+        var n = 2
+        for(i in 1..10)
+        {
+                println(n * i)
+        }
     }
