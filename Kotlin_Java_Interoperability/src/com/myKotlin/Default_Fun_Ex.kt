d6c87169cb5772f101fbef70b820d2728aba82af
ShubhamLokhande2147 <shubhamlokhande2147@gmail.com>
2024-09-30 18:37:40 +0530

    update

--- Kotlin_Java_Interoperability/src/com/myKotlin/Default_Fun_Ex.kt
+++ Kotlin_Java_Interoperability/src/com/myKotlin/Default_Fun_Ex.kt
@@ -9,5 +9,7 @@
         @JvmOverloads  //it used for default argument also load in java file
         fun volume(length : Int , breath : Int , height : Int = 2) : Int //here, specify default argument
         {
+
+
             return length * breath * height
         }
